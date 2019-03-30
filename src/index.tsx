6161b2d60d42ef233e21cfceed8b71c4ed1ab832
Arnaud Huc <arnaud.huc.pro@gmail.com>
2019-03-30 19:22:45 +0100

    Test hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
-import * as _ from "lodash";
+import * as _ from 'lodash';
 import './style.scss';
 
 function component() {
   let element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', '<span class="hello-world">webpack</span>'], ' ');
+  element.innerHTML = _.join(
+    ['Hello', "<span class='hello-world'>webpack</span>"],
+    ' '
+  );
   element.classList.add('hello');
 
   return element;
