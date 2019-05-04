97f7058f9918ad5cc2f4973594914b6459184ede
Arnaud Huc <arnaud.huc.pro@gmail.com>
2019-05-05 00:43:55 +0200

    Edit linter + add tasks for linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   'rules': {
     'indent': [
       'error',
-      2
+      'tab'
     ],
     'linebreak-style': [
       'error',
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,56 @@
 const path = require('path');
 const dirPath = path.resolve(__dirname, 'dist');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.tsx',
-  output: {
-    filename: 'bundle.js',
-    path: dirPath
-  },
-  devtool: 'source-map',
-  devServer: {
-    contentBase: dirPath,
-    compress: true,
-    port: 9000
-  },
-  module: {
-    rules : [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader', // creates style nodes from JS strings
-          'css-loader', // translates CSS into CommonJS
-          'sass-loader' // compiles Sass to CSS, using Node Sass by default
-        ]
-      },
-      {
-        test:/\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(ts|js)x?$/,
-        use: {
-          loader: 'awesome-typescript-loader'
-        }
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: 'source-map-loader'
-      }
-    ]
-  }
+	mode: 'production',
+	entry: './src/index.tsx',
+	output: {
+		filename: 'bundle.js',
+		path: dirPath
+	},
+	devtool: 'source-map',
+	devServer: {
+		contentBase: dirPath,
+		compress: true,
+		port: 9000
+	},
+	module: {
+		rules : [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader', // creates style nodes from JS strings
+					'css-loader', // translates CSS into CommonJS
+					'sass-loader' // compiles Sass to CSS, using Node Sass by default
+				]
+			},
+			{
+				test:/\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+				test: /\.(ts|js)x?$/,
+				use: {
+					loader: 'awesome-typescript-loader'
+				}
+			},
+			{
+				enforce: 'pre',
+				test: /\.js$/,
+				loader: 'source-map-loader'
+			}
+		]
+	},
+	plugins: [new HtmlWebpackPlugin()]
 };
